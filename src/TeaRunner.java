34b834f65441bf27a0bdc3b22f788a5cc3fbded5
U have'nt <whaaqaaaidchbfgvruihgiudfgiufhjifo@gmail.com>
2018-12-08 11:26:20 -0800

    tea

--- src/TeaRunner.java
+++ src/TeaRunner.java
@@ -1,4 +1,8 @@
+import javax.swing.JOptionPane;
 
 public class TeaRunner {
-
+public static void main(String[] args) {
+	String flavor =  JOptionPane.showInputDialog("What flavor of tea do you want?");
+	Teabag teabag = new Teabag(flavor);
+}
 }
